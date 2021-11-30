dfac3df8277d5bdbd9042998fe971d9a9f919ac1
Sissoko <ousmansissoko237@gmail.com>
2021-11-30 16:49:28 +0100

    Create signup.view.php

---
+++ Private/views/signup.view.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Sign_up</title>
+  </head>
+  <body>
+      <div class="text-center mt-3 ">
+        <form action="" method="" style="max-width: 300px; margin:auto;">
+            <img class="mt-5 mb-3" src="/assets/images/logo.png" alt="kzonne logo" width="70px" height="70px">
+            <h2>Please Sign Up....</h2>
+            <input type="text" placeholder="Firstname" class="form-control" id="firstname" name="firstname" >
+            <input type="text" placeholder="lastname" class="form-control mt-3" id="lastname" name="lastname">
+
+            <input type="email" placeholder="Email" class="form-control mt-3" id="email" name="email" >
+            <input type="password" placeholder="PassWord" class="form-control mt-3" id="Password" name="password">
+            <div class="checkbox">
+                <label><input type="checkbox" value="remember_me">Remember me</label>
+            </div>
+            <div class="mt-3">
+            <button type="button" class="btn btn-primary btn-lg btn-block">Sign Up</button>
+            </div>
+        </form>
+    </div>
+    
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+ 
+  </body>
+</html>
